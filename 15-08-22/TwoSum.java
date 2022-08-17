393958a7053b53b9fe9f0a70ef3910d98f6bd897
Rajitha-19 <72659413+Rajitha-19@users.noreply.github.com>
2022-08-17 15:07:13 +0530

    Update TwoSum.java

--- 15-08-22/TwoSum.java
+++ 15-08-22/TwoSum.java
@@ -47,4 +47,25 @@ class Solution {
     }
 }
 
-//
+//approach-2
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+        int[] res=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int val=target-nums[i];
+            if(map.containsKey(val) && map.get(val)!=i){
+                res[0]=i;
+                res[1]=map.get(val);
+            }
+        }
+        return res;
+    }
+}
+
+//Time Complexity-O(n)
+//Space Complexity - O(n)
